acc34fb5e27c194c50482bcd5a651e3114180571
JBB <jbb.prv@gmx.de>
2015-12-27 20:41:38 +0100

    deleted php

--- test/php/xd.php
+++
@@ -1,6 +0,0 @@
-<form action="seite2.php" method="post">
-Geben sie den richtigen Benutzernamen und das richtige Passwort für diese seite ein:<br />
-<input type="Text" name="username" />
-<input type="Password" name="password" />
-<input type="Submit" value="Login" />
-</form> 
